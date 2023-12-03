6057d90103fd96aad8add0ffeb1b4463aa169ae2
Rururuisi <louiseliang9943@gmail.com>
2023-12-02 19:26:40 -0500

    .

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const crypto = require("crypto");
 const querystring = require("querystring");
 const CloseEmitter = require("./module/CloseEmitter.js");
 
-const {client_id, client_secret} = require("./auth/credentials.json");
+const { client_id, client_secret } = require("./auth/credentials.json");
 
 const all_sessions = [];
 const port = 3000;
@@ -15,25 +15,25 @@ const server = http.createServer();
 
 server.on("listening", () => {
     console.log(`Now Listening on Port ${port}`);
-})
+});
 server.listen(port);
 
-
 server.on("request", request_handler);
+
 function request_handler(req, res) {
     console.log(`New Request From ${req.socket.remoteAddress} for ${req.url}`);
     const redirect_uri = `http://${req.headers.host}/auth`;
-    
+
     // route for pipe the form.html to the root page
     if (req.url === "/") {
         const form = fs.createReadStream("html/form.html");
-        res.writeHead(200, { "Content_Type": "text/html" });
+        res.writeHead(200, { Content_Type: "text/html" });
         form.pipe(res);
     }
     // route for collect user input
     else if (req.url.startsWith("/search")) {
         const { keyword } = url.parse(req.url, true).query;
-        if (keyword == null | keyword == "") {
+        if ((keyword == null) | (keyword == "")) {
             error_status(res, 400, "Bad Request", "Empty Search");
             return;
         }
@@ -47,7 +47,11 @@ function request_handler(req, res) {
     else if (req.url.startsWith("/auth")) {
         const { code, state } = url.parse(req.url, true).query;
         const session = all_sessions.find((session) => session.state === state);
-        if (code === undefined || state === undefined || session === undefined) {
+        if (
+            code === undefined ||
+            state === undefined ||
+            session === undefined
+        ) {
             error_status(res, 401, "Unauthorized", "Authorization Denial");
             return;
         }
@@ -60,36 +64,42 @@ function request_handler(req, res) {
     }
 }
 
-
 // clear the token cache when the server close
 const close_emitter = new CloseEmitter();
 close_emitter.on("close", () => {
-    fs.unlink("./cache/token.json", () => { });
+    fs.unlink("./cache/token.json", () => {});
     process.exit();
 });
 
-
-
 // -----------------------Request Food Recipes from TheMealDB--------------------------
-
 
 function get_food_recipes(keyword, state, redirect_uri, res) {
     const endpoint = `https://www.themealdb.com/api/json/v1/1/search.php?s=${keyword}`;
-    https.request(endpoint, { method: "GET" }, (recipes_stream) => {
-        process_stream(recipes_stream, receive_food_recipes, keyword, state, redirect_uri, res)
-    }).end();
+    https
+        .request(endpoint, { method: "GET" }, (recipes_stream) => {
+            process_stream(
+                recipes_stream,
+                receive_food_recipes,
+                keyword,
+                state,
+                redirect_uri,
+                res
+            );
+        })
+        .end();
 }
 
 function receive_food_recipes(body, keyword, state, redirect_uri, res) {
-    const {meals} = JSON.parse(body);
-    if (meals == null) {   // no search result
+    const { meals } = JSON.parse(body);
+    if (meals == null) {
+        // no search result
         error_status(res, 404, "Not Found", "No Items Match Your Search");
         return;
     }
 
     // obtain info that we need for creating the recipes
     const food_recipes = [];
-    meals.forEach(meal => {
+    meals.forEach((meal) => {
         // integrate the measurement and ingredients
         let ingre = getValsByKeys(meal, "strIngredient");
         let measure = getValsByKeys(meal, "strMeasure");
@@ -101,13 +111,13 @@ function receive_food_recipes(body, keyword, state, redirect_uri, res) {
             ingredients: strIngre,
             instructions: arrInstructions,
             image: meal.strMealThumb,
-            video: meal.strYoutube
+            video: meal.strYoutube,
         };
         food_recipes.push(recipe);
     });
 
     // save food_recipes into current session of all_sessions array
-    const index = all_sessions.findIndex(session => session.state === state);
+    const index = all_sessions.findIndex((session) => session.state === state);
     all_sessions[index].food_recipes = food_recipes;
 
     // if we have available token, skip the authentication of Notion API
@@ -122,19 +132,16 @@ function receive_food_recipes(body, keyword, state, redirect_uri, res) {
     });
 }
 
-
-
 // ---------------Notion OAuth 2.0 Three Legged Authentication------------------
-
 
 function redirect_to_Notion_auth(state, redirect_uri, res) {
     const auth_endpoint = "https://api.notion.com/v1/oauth/authorize";
     const authQuery = {
-        "client_id": client_id,
-        "redirect_uri": redirect_uri,
-        "response_type": "code",
-        "owner": "user",
-        "state": state
+        client_id: client_id,
+        redirect_uri: redirect_uri,
+        response_type: "code",
+        owner: "user",
+        state: state,
     };
     let queryParams = querystring.stringify(authQuery);
     res.writeHead(302, { Location: `${auth_endpoint}?${queryParams}` }).end();
@@ -145,75 +152,91 @@ function request_access_token(authCode, res, redirectUri, session) {
     const post_data = JSON.stringify({
         grant_type: "authorization_code",
         code: authCode,
-        redirect_uri: redirectUri
+        redirect_uri: redirectUri,
     });
-    let auth_base64 = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
+    let auth_base64 = Buffer.from(`${client_id}:${client_secret}`).toString(
+        "base64"
+    );
     const options = {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            Authorization: `Basic ${auth_base64}`
-        }
+            Authorization: `Basic ${auth_base64}`,
+        },
     };
-    https.request(token_endpoint, options, (token_stream) => {
-        process_stream(token_stream, receive_access_token, session, res)
-    }).end(post_data);
+    https
+        .request(token_endpoint, options, (token_stream) => {
+            process_stream(token_stream, receive_access_token, session, res);
+        })
+        .end(post_data);
 }
 
 function receive_access_token(body, session, res) {
     const { access_token } = JSON.parse(body);
-    if (access_token === undefined) {     // when received error info
+    if (access_token === undefined) {
+        // when received error info
         error_status(res, 401, "Unauthorized", "Failed to Get Access Token");
         return;
     }
-    fs.writeFile("./cache/token.json", body, (err) => { });     // cache the token for further requests
-    create_keyword_page(access_token, session.food_recipes, session.keyword, res);
+    fs.writeFile("./cache/token.json", body, (err) => {}); // cache the token for further requests
+    create_keyword_page(
+        access_token,
+        session.food_recipes,
+        session.keyword,
+        res
+    );
 }
-
-
 
 // ------------------Create Food Recipes on Notion------------------------
 
 function create_keyword_page(token, food_recipes, keyword, res) {
-    const keyword_form = require("./workpages/keyword_page.json");   // require calls are cached, consecutive calls will return the same object
-    const keyword_page = JSON.parse(JSON.stringify(keyword_form));   // make a deep copy of the keyword_form
-    keyword_page.properties.title[0] = { "text": { "content": `${keyword}` } };
+    const keyword_form = require("./workpages/keyword_page.json"); // require calls are cached, consecutive calls will return the same object
+    const keyword_page = JSON.parse(JSON.stringify(keyword_form)); // make a deep copy of the keyword_form
+    keyword_page.properties.title[0] = { text: { content: `${keyword}` } };
     const post_data = JSON.stringify(keyword_page);
     let recipe_count = 0;
-    create_notion_page(token, post_data, add_recipes, food_recipes, recipe_count, res);
+    create_notion_page(
+        token,
+        post_data,
+        add_recipes,
+        food_recipes,
+        recipe_count,
+        res
+    );
 }
 
 function add_recipes(body, token, food_recipes, count, res) {
     const parent_page = JSON.parse(body);
-    if (parent_page.object === 'error') {
+    if (parent_page.object === "error") {
         error_status(res, 400, "Bad Request", "Failed to Create Keyword Page");
         return;
     }
     process_recipe(parent_page, token, food_recipes, count, res);
 }
 
-
 function process_recipe(parent_page, token, food_recipes, count, res) {
     const parent_id = parent_page.id;
     const recipe = food_recipes[count];
 
     // create recipe page inside the keyword page for each recipe
-    const recipe_form = require("./workpages/recipe_page.json");    // require calls are cached, consecutive calls will return the same object
-    const recipe_page = JSON.parse(JSON.stringify(recipe_form));    // make a deep copy of the recipe_form
+    const recipe_form = require("./workpages/recipe_page.json"); // require calls are cached, consecutive calls will return the same object
+    const recipe_page = JSON.parse(JSON.stringify(recipe_form)); // make a deep copy of the recipe_form
 
     // setting params for the page
     recipe_page.parent.page_id = parent_id;
-    recipe_page.properties.title[0] = { "text": { "content": `${recipe.name}` } };
-    recipe_page.children[3].paragraph.rich_text[0] = { "text": { "content": recipe.ingredients } };
+    recipe_page.properties.title[0] = { text: { content: `${recipe.name}` } };
+    recipe_page.children[3].paragraph.rich_text[0] = {
+        text: { content: recipe.ingredients },
+    };
 
-    recipe.instructions.forEach(instruction => {
+    recipe.instructions.forEach((instruction) => {
         recipe_page.children.push({
-            "paragraph": {
-                "rich_text": [{ "text": { "content": `${instruction}` } }]
-            }
+            paragraph: {
+                rich_text: [{ text: { content: `${instruction}` } }],
+            },
         });
     });
-    
+
     // if image url is not provided, remove "cover"
     if (recipe.image.startsWith("http")) {
         recipe_page.cover.external.url = recipe.image;
@@ -224,15 +247,29 @@ function process_recipe(parent_page, token, food_recipes, count, res) {
     if (recipe.video.startsWith("http")) {
         recipe_page.children[1].video.external.url = recipe.video;
     } else {
-        recipe_page.children.splice(0, 2);      // remove the first 2 elements from children
+        recipe_page.children.splice(0, 2); // remove the first 2 elements from children
     }
 
     const post_data = JSON.stringify(recipe_page);
-    create_notion_page(token, post_data, wait_for_page_response, parent_page, food_recipes, count, res);
+    create_notion_page(
+        token,
+        post_data,
+        wait_for_page_response,
+        parent_page,
+        food_recipes,
+        count,
+        res
+    );
 }
 
-
-function wait_for_page_response(body, token, parent_page, food_recipes, count, res) {
+function wait_for_page_response(
+    body,
+    token,
+    parent_page,
+    food_recipes,
+    count,
+    res
+) {
     count++;
     if (count == food_recipes.length) {
         res.writeHead(302, { Location: parent_page.url }).end();
@@ -241,40 +278,37 @@ function wait_for_page_response(body, token, parent_page, food_recipes, count, r
     }
 }
 
-
-
 // ---------------------Reusable Assistant Function------------------------
 
 // used when there's error or the user visit the page that does not exist
 function error_status(res, status_code, status, info) {
-    res.writeHead(status_code, { "Content_Type": "text/html" });
+    res.writeHead(status_code, { Content_Type: "text/html" });
     res.write(`<h1>${status_code} ${status}</h1>`);
-    res.end(`<p>${info}</p>`)
+    res.end(`<p>${info}</p>`);
 }
-
 
 function process_stream(stream, callback, ...args) {
     let body = "";
-    stream.on("data", chunk => body += chunk);
+    stream.on("data", (chunk) => (body += chunk));
     stream.on("end", () => callback(body, ...args));
 }
-
 
 function create_notion_page(token, post_data, callback, ...args) {
     const page_endpoint = "https://api.notion.com/v1/pages";
     const options = {
         method: "POST",
         headers: {
-            "Authorization": `Bearer ${token}`,
+            Authorization: `Bearer ${token}`,
             "Content-Type": "application/json",
-            "Notion-Version": "2022-06-28"
-        }
+            "Notion-Version": "2022-06-28",
+        },
     };
-    https.request(page_endpoint, options, (page_stream) => {
-        process_stream(page_stream, callback, token, ...args)
-    }).end(post_data);
+    https
+        .request(page_endpoint, options, (page_stream) => {
+            process_stream(page_stream, callback, token, ...args);
+        })
+        .end(post_data);
 }
-
 
 // get all values that the keys name starts with a specific pattern, return as array
 function getValsByKeys(pairArr, pattern) {
@@ -289,7 +323,6 @@ function getValsByKeys(pairArr, pattern) {
     }
     return vals;
 }
-
 
 // combine elements of two arrays to strings in index order
 function combine_arr_to_str(arr1, arr2) {
